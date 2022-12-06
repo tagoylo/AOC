e242d91962d3250d1a295c44203e0664d99231c9
Trecia Agoylo <trecia.agoylo@analog.com>
2022-12-06 14:50:35 +0800

    Optimize day6
    Signed-off-by: Trecia Agoylo <trecia.agoylo@analog.com>

--- 2022/Day6/solution.py
+++ 2022/Day6/solution.py
@@ -1,36 +1,14 @@
-import re          
-
-def split_on_empty_lines(s):
-
-    blank_line = r"(?:\r?\n){2,}"
-    two_blank_line = r"(?:\r?\n){3,}"
-    new_line = r"\r\n|\r|\n"
-    character = r"[a]"
-
-    return re.split(new_line, s.strip())
-
 f= open('input.txt', 'r').read()
-#elves=split_on_empty_lines(f)
-#print(f)
 
-i=0
-for i in range(0,len(f)):
-    start = i
-    end = i+4
-    marker = f[start:end]
-    #print(marker)
-    if len(set(marker)) == 4:
-        #print(marker[-1:])
-        print("Part1: "+str(end))
-        break
+def get_char_count(part,n):
+    i=0
+    for i in range(0,len(f)):
+        marker = f[i:i+n]
+        if len(set(marker)) == n:
+            break
+    return print("Part " +str(part)+": "+str(i+n))
 
-i=0
-for i in range(0,len(f)):
-    start = i
-    end = i+14
-    marker = f[start:end]
-    #print(marker)
-    if len(set(marker)) == 14:
-        #print(marker[-1:])
-        print("Part2: "+str(end))
-        break
+#part1
+get_char_count(1, 4)
+#part2
+get_char_count(2, 14)
